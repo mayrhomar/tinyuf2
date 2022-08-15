bc3dafe54cbf78bf4943ff53de84f6ca6aff758c
Martin <mayrhomar@gmail.com>
2022-08-15 14:33:02 +0200

    Sensorboard changes

--- ports/espressif/boards/waveshare_esp32_s2_pico_lcd/board.h
+++ ports/espressif/boards/waveshare_esp32_s2_pico_lcd/board.h
@@ -29,12 +29,24 @@
 // Button
 //--------------------------------------------------------------------+
 
+// Enter UF2 mode if GPIO is pressed while 2nd stage bootloader indicator
+// is on e.g RGB = Purple. If it is GPIO0, user should not hold this while
+// reset since that will instead run the 1st stage ROM bootloader
 #define PIN_BUTTON_UF2        0
+
+// GPIO that implement 1-bit memory with RC components which hold the
+// pin value long enough for double reset detection.
+#define PIN_DOUBLE_RESET_RC	  34
 
 //--------------------------------------------------------------------+
 // LED
 //--------------------------------------------------------------------+
-
+// GPIO connected to Neopixel data
+#define NEOPIXEL_PIN          37
+// Brightness percentage from 1 to 255
+#define NEOPIXEL_BRIGHTNESS   255
+// Number of neopixels
+#define NEOPIXEL_NUMBER       1
 
 // #define LED_PIN               9
 // #define LED_STATE_ON          0
